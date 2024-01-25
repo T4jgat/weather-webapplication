3d79028bfec8cb7ba67a6fa0201e92eec2a320f1
t4jgat <tmtmtm1423@gmail.com>
2024-01-25 22:42:20 +0600

    Some refactorings

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -12,12 +12,14 @@ class weatherController {
                 const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.WEATHER_API_KEY}&units=metric`
                 const weatherResponse = await fetch(WEATHER_API_URL);
                 const weatherData = await weatherResponse.json()
-                console.log("===== WEATHER ======")
-                console.log(weatherData)
 
                 const AIR_POLLUTION_API_URL = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&appid=${process.env.WEATHER_API_KEY}`
                 const airPollutionResponse = await fetch(AIR_POLLUTION_API_URL)
                 const airPollutionData = await airPollutionResponse.json()
+
+                console.log("===== WEATHER ======")
+                console.log(weatherData)
+
                 console.log("===== AIR =====")
                 console.log(airPollutionData)
 
